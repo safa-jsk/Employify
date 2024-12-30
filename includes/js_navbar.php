060b74c642d3fbad8a21cdbea571e650d93c5b7e
Jabir Safa Khandoker <jabirkhandoker03@gmail.com>
2024-12-30 08:42:26 +0600

    Navbar DB connection

--- includes/js_navbar.php
+++ includes/js_navbar.php
@@ -1,13 +1,5 @@
 <?php
-$server = "localhost";
-$user = "root";
-$pass = "";
-$db = "employify";
-
-$con = mysqli_connect($server, $user, $pass, $db);
-if (!$con) {
-    die("Error" . mysqli_connect_error());
-}
+require_once 'DBconnect.php';
 
 session_start();
 
