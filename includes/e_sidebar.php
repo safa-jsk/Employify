94efd82370fc9fb63e42e367c056ff6fb4b371dc
Jabir Safa Khandoker <jabirkhandoker03@gmail.com>
2025-01-01 03:16:14 +0600

    Fixed Employer Sidebar

--- includes/e_sidebar.php
+++ includes/e_sidebar.php
@@ -1,10 +1,8 @@
 <aside class="sidebar">
     <ul>
         <li><a href="e_dashboard.php">Dashboard</a></li>
-
         <li><a href="e_posted-jobs.php">Posted Jobs</a></li>
         <li><a href="e_applied.php">Applied Candidates</a></li>
-        <li><a href="e_posted-jobs.php">Posted Jobs</a></li>
         <li><a href="e_shortlisted.php">Shortlisted Candidates</a></li>
         <li><a href="e_search-candidates.php">All Candidates</a></li>
     </ul>
