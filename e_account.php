e664b8def16df96c84c28e3d99ba02438d70851b
Jabir Safa Khandoker <jabirkhandoker03@gmail.com>
2025-01-03 20:00:10 +0600

    Fixed account access

--- e_account.php
+++ e_account.php
@@ -3,8 +3,10 @@ session_start();
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 require_once 'DBconnect.php';
 
-if (!isset($_SESSION['username']) || $_SESSION['role'] != 'job_seeker') {
-    header("Location: e_account.php");
+// Ensure correct user is logged in
+$pageRole = 'employer';
+if (!isset($_SESSION['username']) || $_SESSION['role'] !== $pageRole) {
+    echo "<script>alert('You must log in first!'); window.location.href = 'index.php';</script>";
     exit;
 }
 ?>
